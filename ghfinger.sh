baf6834cf26e376e1ec250d339aa27dbf079eedc
Manfred Touron <m@42.am>
2013-07-28 00:23:49 +0200

    added api data

--- ghfinger.sh
+++ ghfinger.sh
@@ -2,4 +2,17 @@
 
 NAME="$1"
 
-curl -s "https://raw.github.com/$NAME/ghfinger/master/.plan"
+curl_default() {
+    url="$1"
+    default="$2"
+    curl -s --head "$url" | head -n 1 | grep 404 >/dev/null && echo "$default" || curl -s "$url"
+}
+
+echo "github info"
+curl -s "https://api.github.com/users/$NAME"
+
+echo ".plan file"
+curl_default "https://raw.github.com/$NAME/ghfinger/master/.plan" "No .plan file"
+
+echo ".project file"
+curl_default "https://raw.github.com/$NAME/ghfinger/master/.project" "No .project file"
