bf6e09206ed6ecd98f0da4b47cce60185aea5eaa
Manfred Touron <m@42.am>
2013-07-29 23:09:46 +0200

    added user.json support

--- ghfinger.sh
+++ ghfinger.sh
@@ -16,6 +16,9 @@ curl_default() {
 echo "github info"
 curl -s "https://api.github.com/users/$NAME"
 
+echo "user.json file"
+curl_default "https://raw.github.com/$NAME/ghfinger/master/user.json" "No user.json file"
+
 echo ".plan file"
 curl_default "https://raw.github.com/$NAME/ghfinger/master/.plan" "No .plan file"
 
